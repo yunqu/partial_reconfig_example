a745e1adfacc4998e7c640e39187da56671bbfa3
root <yunq@xilinx.com>
2019-01-15 18:00:51 +0000

    copy bitstream and check timing

--- boards/ZCU104/gpio_pr/build_bitstream.tcl
+++ boards/ZCU104/gpio_pr/build_bitstream.tcl
@@ -20,9 +20,14 @@ if {$proj_pr_flow == 1} {
 # implementation
 launch_runs impl_1 -to_step write_bitstream -jobs 8
 wait_on_run impl_1
+file copy -force ./${overlay_name}/${overlay_name}.runs/impl_1/${design_name}_wrapper.bit ${overlay_name}.bit
 
 set child_impl_runs [get_runs child_*]
 foreach  { impl_run } $child_impl_runs  {
-   launch_runs $impl_run -to_step write_bitstream -jobs 4
-   wait_on_run $impl_run
+    launch_runs $impl_run -to_step write_bitstream -jobs 4
+    wait_on_run $impl_run
+    set bits [glob -type f ./${overlay_name}/${overlay_name}.runs/${impl_run}/*_partial.bit]
+    foreach { bit } $bits {
+        file copy -force $bit ./
+    }
 }
